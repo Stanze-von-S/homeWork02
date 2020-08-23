951e5efe12fc4b879f87b36b35c05e2dfc09e27a
Konstantin Mamonov <stanze.von.saidenberg@yandex.ru>
2020-08-23 18:02:18 +0300

    added css-loader, style-loader and header.css

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import styles from './header.css';
 
 export function Header() {
   return (
     <header>
-      <h1>Hello, React</h1>
+      <h1 className={styles.example}>Hello, React</h1>
     </header>
   );
 }
